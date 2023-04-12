c451f573d8b9551e552153ab43b253cb83aac662
Praveen Gajulapalle <Praveen.gaju8994@gmail.com>
2023-04-12 18:35:36 +0530

    updating learn-terraform directory

--- 04-variable/var.tf
+++ 04-variable/var.tf
@@ -5,3 +5,16 @@ variable "sample" {
 output "sample" {
   value = var.sample
 }
+
+variable "sample1" {} //declaration of variable
+
+output "sample1" {
+  value = var.sample1
+}
+
+variable "cli" {}
+
+output "cli" {
+  value = var.cli
+}
+
