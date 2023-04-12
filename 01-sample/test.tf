e9fb5e326bba30157c9b2b34e940110dcda98e0a
Praveen Gajulapalle <Praveen.gaju8994@gmail.com>
2023-04-12 13:16:07 +0530

    updating learn-terraform directory

--- 01-sample/test.tf
+++ 01-sample/test.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "ec2" {
-  ami                    = "ami-0a017d8ceb274537d"
-  instance_type          = "t3.micro"
-  vpc_security_group_ids = ["sg-0fc69f59ac03ac238"]
+  ami = "ami-0089b8e98cd95257d"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-0149e76eef2a1cd6a"]
   tags = {
-    Name = "test"
+    name = "test"
   }
 }
