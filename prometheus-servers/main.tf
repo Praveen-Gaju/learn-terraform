ccc62103152c9bab672b718c6e26a1a340d8a5d3
Praveen Gajulapalle <Praveen.gaju8994@gmail.com>
2023-05-16 18:07:29 +0530

    updating learn-terraform directory

--- prometheus-servers/main.tf
+++ prometheus-servers/main.tf
@@ -11,6 +11,12 @@ resource "aws_spot_instance_request" "node1" {
 }
 
 
+resource "aws_ec2_tag" "node1" {
+  resource_id = aws_spot_instance_request.node1.spot_instance_id
+  key         = "Name"
+  value       = "prom-test-server"
+}
+
 resource "aws_spot_instance_request" "node2" {
   ami                     = "ami-04d9ca43b6449e12a"
   vpc_security_group_ids  = ["sg-0149e76eef2a1cd6a"]
@@ -20,4 +26,10 @@ resource "aws_spot_instance_request" "node2" {
   tags = {
     Name = "prom-test-node"
   }
+}
+
+resource "aws_ec2_tag" "node2" {
+  resource_id = aws_spot_instance_request.node2.spot_instance_id
+  key         = "Name"
+  value       = "prom-test-node"
 }
