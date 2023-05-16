c86fc8338fb98ddce18f13d2954ac670d76a7824
Praveen Gajulapalle <Praveen.gaju8994@gmail.com>
2023-05-16 18:02:06 +0530

    updating learn-terraform directory

---
+++ prometheus-servers/main.tf
@@ -0,0 +1,23 @@
+# Request a spot instance
+resource "aws_spot_instance_request" "node1" {
+  ami                     = "ami-04d9ca43b6449e12a"
+  vpc_security_group_ids  = ["sg-0149e76eef2a1cd6a"]
+  instance_type           = "t2.micro"
+  wait_for_fulfillment    = true
+
+  tags = {
+    Name = "prom-test-server"
+  }
+}
+
+
+resource "aws_spot_instance_request" "node2" {
+  ami                     = "ami-04d9ca43b6449e12a"
+  vpc_security_group_ids  = ["sg-0149e76eef2a1cd6a"]
+  instance_type           = "t2.micro"
+  wait_for_fulfillment    = true
+
+  tags = {
+    Name = "prom-test-node"
+  }
+}
